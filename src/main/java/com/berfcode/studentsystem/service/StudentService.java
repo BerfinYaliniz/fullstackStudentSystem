db5ac355e1259bc7d9e65ba3d53795a0df2c6e0b
Berfin22 <berfinyaliniz1@hotmail.com>
2023-02-09 23:48:39 +0300

    completed backend

--- src/main/java/com/berfcode/studentsystem/service/StudentService.java
+++ src/main/java/com/berfcode/studentsystem/service/StudentService.java
@@ -1,4 +1,11 @@
 package com.berfcode.studentsystem.service;
 
+import com.berfcode.studentsystem.model.Student;
+
+import java.util.List;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+
 }
--- src/main/java/com/berfcode/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/berfcode/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,24 @@
 package com.berfcode.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.berfcode.studentsystem.model.Student;
+import com.berfcode.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
